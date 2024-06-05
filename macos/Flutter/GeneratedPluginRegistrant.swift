c2aa3c479b7fda09c96c7556bd41a35c09b7485a
Sardor <sardorwox77@gmail.com>
2024-06-05 19:43:28 +0500

    first commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_inappwebview_macos
+import shared_preferences_foundation
+import youtube_dl
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  YoutubeDlPlugin.register(with: registry.registrar(forPlugin: "YoutubeDlPlugin"))
+}
